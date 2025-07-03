da638965b491bb0e2af3ae824199fe6741fa41f1
Hasibullah-cyber <hasdibsolder506@gmail.com>
2025-07-03 16:15:25 +0600

    Update page.tsx

--- Order-confirmation/page.tsx
+++ Order-confirmation/page.tsx
@@ -1,17 +1,7 @@
-'use client'
-interface OrderData {
-  orderId: string
-  trackingNumber: string
-  customerName: string
-  customerEmail: string
-  totalAmount: number
-  paymentMethod: string
-  estimatedDelivery: string
-  items: Array<{
-    id: string
-    name: string
-    price: number
-    quantity: number
-    image?: string
-  }>
-}
+<html>
+       <p>
+       payment confirmation successful 
+      </p>
+
+</html>
+      
